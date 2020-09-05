fb8f125b55499a577552b7553d64670213c33f3f
EstebanRestoy <esteban.restoy@etu.uca.fr>
2020-09-05 09:17:06 +0200

    [ADD] React Hook for room

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+const ENDPOINT = "localhost:5000";
+let socket;
 
-const Room = () => {
-    return (
-        <h1> Room : XX </h1>
-    )
-}
+const Room = ({ location }) => {
+  const [tag, setTag] = useState("");
+  const [room, setRoom] = useState("");
+  useEffect(() => {
+    const { tag, room } = queryString.parse(location.search);
+    socket = io(ENDPOINT);
 
-export default Room
+    setTag(tag);
+    setRoom(room);
+
+    socket.emit("join", { tag, room });
+  }, [ENDPOINT, location.search]);
+  return <h1> Room : XX </h1>;
+};
+
+export default Room;
